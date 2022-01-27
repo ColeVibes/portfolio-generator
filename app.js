51662dd3eaa573a963db5b586e049a9500e924f5
J.Cole <ccjohn015@gmail.com>
2022-01-26 18:22:19 -0600

    added when

--- app.js
+++ app.js
@@ -33,9 +33,22 @@ const promptUser = () => {
       message: 'Enter your GitHub Username'
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Woudl you like to provide info about yourself?',
+      default: true
+    },
+    {
       type: 'input',
       name: 'about',
-      message: 'Provide some information about yourself:'
+      message: 'Provide some information about yourself:',
+      when: ({confirmAbout}) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
   ]);
 };
